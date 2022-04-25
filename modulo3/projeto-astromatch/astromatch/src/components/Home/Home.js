386f94de84f1403ed59fbb50e706f2aaa4efecce
chriss3008 <christian-cardeal@hotmail.com>
2022-04-24 21:56:54 -0300

    projeto finalizado

--- modulo3/projeto-astromatch/astromatch/src/components/Home/Home.js
+++ modulo3/projeto-astromatch/astromatch/src/components/Home/Home.js
@@ -1,17 +1,71 @@
 import React, { useState, useEffect} from "react";
 import axios from "axios";
-import { CardContainer, CardHeader, UserPhoto, CardPhoto, CardFooter, CommentContainer } from './styles'
+import { CardContainer} from './styles'
 
 const Home = (props) => {
+    const [prof, setProf] = useState({})
+
+    useEffect(() => {
+        getProf()
+      }, [])
+
+
+    const getProf = () => {
+        const url = 'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/christian-silveira/person'
+            axios
+                .get(url)
+                .then((response) => {
+                    setProf(response.data.profile)
+                })
+                .catch((error) => {
+                    alert(error.response.data)
+                })        
+    }
+
+    const acceptProf = () => {
+        const url = 'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/christian-silveira/choose-person'
+        const body = {
+            id: prof.id,
+            choice: true
+        }
+            axios
+                .post(url, body)
+                .then((response) => {
+                    getProf()
+                })
+                .catch((error) => {
+                    alert(error.message)
+                })        
+    }
+
+    const refuseProf = () => {
+        const url = 'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/christian-silveira/choose-person'
+        const body = {
+            id: prof.id,
+            choice: false
+        }
+            axios
+                .post(url, body)
+                .then((response) => {
+                    getProf()
+                })
+                .catch((error) => {
+                    alert(error.message)
+                })        
+    }
+
+
     return (
         <CardContainer>
             matchmatch
             <button onClick={props.gotoProf}>Profile</button>
             <br/>
-CHAMAR COMPONENTE CARD
+            <img src={prof.photo} alt={prof.name} width= "300vw"  height = "300vh" />
+          <p> {prof.name}, {prof.age} </p>
+          <p>{prof.bio}</p>
 <br/>
-            <button>X</button>
-            <button>S2</button>
+            <button onClick={refuseProf}>X</button>
+            <button onClick={acceptProf}>S2</button>
 
 
 
--- modulo3/projeto-astromatch/astromatch/src/components/Prof/Prof.js
+++ modulo3/projeto-astromatch/astromatch/src/components/Prof/Prof.js
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { CardContainer, CardHeader, UserPhoto, CardPhoto, CardFooter, CommentContainer } from './styles'
+import { CardContainer} from './styles'
 
 
 const Prof = (props) => {
+
+    const [likes, setLikes] = useState([]);
+
+    useEffect(() => {
+        getList()
+    }, []);
+
+    const getList = () => {
+        const url = 'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/christian-silveira/matches'
+        axios
+            .get(url)
+            .then((response) => {
+                setLikes(response.data.matches)
+            })
+            .catch((error) => {
+                alert(error.response.data)
+            })
+    }
+
     const clearAll = () => {
         const url = 'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/christian-silveira/clear'
         if (window.confirm(`Do you really want to clear your matches?`)) {
@@ -18,19 +37,28 @@ const Prof = (props) => {
         }
     }
 
+    const allLikes = likes.map((prof) => {
+        return (
+            <>
+                <img src={prof.photo} alt={prof.name} width= "65vw"  height = "65vh"  />
+            {prof.name}
+            <br/>
+            </>
+        )
+      })
 
 
 
-    return (
-        <CardContainer>
-            <button onClick={props.gotoHome}>Profile</button>
-            matchmatch
-            <button onClick={clearAll} >Clear all mathes</button>
-            Lista renderizada de profiles
-        </CardContainer>
-    )
-}
 
+        return (
+            <CardContainer>
+                <button onClick={props.gotoHome}>Profile</button>
+                matchmatch
+                <button onClick={clearAll} >Clear all mathes</button>
+                <br/> {allLikes}
+            </CardContainer>
+        )
+    }
 export default Prof
 
 
