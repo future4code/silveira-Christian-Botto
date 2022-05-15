2174aefe811cacf2f35a84ad2957fee0c9d627b9
chriss3008 <christian-cardeal@hotmail.com>
2022-05-15 15:33:06 -0300

    eee

--- modulo3/projeto-labeddit/labeddit/src/pages/FeedPage/FeedPage.js
+++ modulo3/projeto-labeddit/labeddit/src/pages/FeedPage/FeedPage.js
@@ -1,17 +1,34 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import PostCard from "../../components/PostCard/PostCard";
 import { BASE_URL } from "../../constants/urls";
 import { useProtectedPage } from "../../hooks/useProtectedPage";
 import { useRequestData } from "../../hooks/useRequestData";
+import { goToPost } from "../../routes/coordinator";
 
-const FeedPage = () => {
+const FeedPage = () => {    
     useProtectedPage()
+    const navigate = useNavigate()
     // const feed_aux = useRequestData([],`${BASE_URL}/posts`)
     // const feed = feed_aux[0]
+
     const [feed] = useRequestData([],`${BASE_URL}/posts`)
 
+
+    const onClickPost = (id) => {
+        goToPost(navigate,id)
+        console.log("clicou")
+    }
+
     const listFeed = feed.map ((post) => {
-        return ( <p>{post.title} </p>)
+        return ( 
+        <PostCard
+        key = {post.id}
+            title={post.title} 
+            onClick = {onClickPost}
+            id = {post.id}
+           />
+        )
     } )
     
 
