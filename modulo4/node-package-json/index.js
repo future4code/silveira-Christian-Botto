1037a4a2a5ef835d6fbd335c718211bd66c77dc1
chriss3008 <christian-cardeal@hotmail.com>
2022-05-24 16:40:23 -0300

    24.05.2022

---
+++ modulo4/node-package-json/index.js
@@ -0,0 +1,23 @@
+// 1 - 
+
+// a) Podemosacessar os parâmetros com o objeto process.argv, sendo que os dois primeiros elementos são fixos.
+
+// b)
+// const userName = process.argv[2]
+// const userAge = Number(process.argv[3])
+// console.log(`Olá, ${userName}! Você tem ${userAge} anos.`)
+
+// c)
+// const userName = process.argv[2]
+// const userAge = Number(process.argv[3])
+// const userAgeFuture = userAge + 7
+// console.log(`Olá, ${userName}! Você tem ${serAge} anos. Em sete anos você terá ${serAgeFuture}`)
+
+// 2 - 
+// Há apenas uma função para rodar todos os casos.
+// O terceiro elemento é o primeiro número, o quarto elemento é o segundo número.
+// O quinto elemento é o tipo de operação:
+// add = somar
+// sub = subtrair
+mult = multiplicar
+// div = dividir
