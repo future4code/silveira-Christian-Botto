028d3e50bd932d27ce77428de4b5c32d937e0974
chriss3008 <christian-cardeal@hotmail.com>
2022-05-26 15:57:34 -0300

    coreccao

---
+++ modulo4/aprofundamento-typescript/ex1.ts
@@ -0,0 +1,53 @@
+// 1 - 
+// a) 
+// const minhaString:string = 5
+// Me dá um erro dizendo que o tipo number não está sendo recebida por string.
+
+// b)
+// const meuNumero:number | string = "oi"
+// Utilizando a barra vertical | e declarando o outro tipo que aceita
+
+// c)
+// const obj1: { nome: string, idade: number, corFavorita: string } = {
+//     nome: "",
+//     idade: ,
+//     corFavorita: ""
+// }
+
+// const obj2: { nome: string, idade: number, corFavorita: string } = {
+//     nome: "",
+//     idade: ,
+//     corFavorita: ""
+// }
+
+// const obj3: { nome: string, idade: number, corFavorita: string } = {
+//     nome: "",
+//     idade: ,
+//     corFavorita: ""
+// }
+
+// const ob4j: { nome: string, idade: number, corFavorita: string } = {
+//     nome: "",
+//     idade: ,
+//     corFavorita: ""
+// }
+
+// type pessoa { nome: string, idade: number, corFavorita: string }
+// const ob5:pessoa ={
+//     nome: "",
+//     idade: ,
+//     corFavorita: ""
+// }
+
+// d)
+enum corArcoIris {
+    vermelha = "vermelho",
+    laranja = "laranja",
+    amarela = "amarelo",
+    verde = "verde",
+    azul = "axul",
+    azul_escuro = "azul-escuro",
+    violeta = "violeta"
+}
+
+
