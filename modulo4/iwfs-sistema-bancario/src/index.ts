473ae94dc3ee4f6ce97c541ba23cebb929edd847
chriss3008 <christian-cardeal@hotmail.com>
2022-06-05 20:42:17 -0300

    na metade

--- modulo4/iwfs-sistema-bancario/src/index.ts
+++ modulo4/iwfs-sistema-bancario/src/index.ts
@@ -1,6 +1,6 @@
-// dentro do index.ts, fazer importações e ativar o Express e Cors.
 
-import express, {Express} from 'express'
+import express, { Express } from 'express'
+import { Request, Response } from "express";
 import cors from 'cors'
 
 const app: Express = express();
@@ -8,11 +8,69 @@ const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
-// a função express() inicia a aplicação web com express
-// os .use() ativam os módulos de Bodyparser e Cors
+// 1-
+type user = {
+    name: string;
+    cpf: number;
+    birth: string;
+};
 
-// esse código + essa importação para criar o servidor:
-// por performance, é bom o servidor ser o último trecho de código do documento
+// 2-
+let users: user[] = []
+
+// 3-
+type transfer = {
+    nameSend: string;
+    cpfSend: number;
+    nameReceive: string;
+    cpfReceive: number;
+    amount: number;
+};
+
+// 4- Mão Sei
+
+
+// 5 and 8-
+app.post("/users", (req: Request, res: Response) => {
+    let errorCode: number = 400;
+    try {
+      const { name, cpf, birth } = req.body;  
+      if (!name || !cpf || !birth) {
+        errorCode = 422;
+        throw new Error("Please check the fields!");
+      }  
+      // Age Verification
+      let age:number = 2022 - Number(birth.slice(6,9)); // DD/MM/YYYY
+      if (age<18) { 
+        errorCode = 444;
+        throw new Error("You must be over 18 years of age");
+      }  
+      //
+      const newUser: user = {
+        name,
+        cpf,
+        birth,
+      };  
+      users.push(newUser);  
+      res.status(201).send({ message: "User created successefully" });
+    } catch (error: any) {
+      res.status(errorCode).send({ messagem: error.message });
+    }
+  });
+
+  // 6-
+  app.get("/users", (req: Request, res: Response) => {
+    let errorCode: number = 400;
+    try {
+      res.status(200).send(users);
+    } catch (error: any) {
+      res.status(errorCode).send({ message: error.message });
+    }
+  });
+
+
+
+
 
 const server = app.listen(3003, () => {
     console.log(`Server is running in http://localhost:3003`);
