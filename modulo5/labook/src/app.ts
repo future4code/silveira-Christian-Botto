3a8c6862d96dae354ef8f4489cb07aa9413bb8cd
chriss3008 <christian-cardeal@hotmail.com>
2022-07-08 17:12:46 -0300

    sexta

--- modulo5/labook/src/app.ts
+++ modulo5/labook/src/app.ts
@@ -1,17 +1,21 @@
-import express from "express";
+import express, { Express } from "express";
+import knex from "knex";
 import cors from "cors";
+import dotenv from "dotenv";
 import { AddressInfo } from "net";
 
-export const app = express();
+dotenv.config();
 
+const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
 const server = app.listen(process.env.PORT || 3003, () => {
-    if(server) {
+    if (server) {
         const address = server.address() as AddressInfo;
-        console.log(`Server is running in http://localhost:${address.port}`);
+        console.log(`Server is running in http://localhost:
+${address.port}`);
     } else {
-        console.error(`Faiilure upon starting server.`)
+        console.error(`Failure upon starting server.`);
     }
-})
+});
