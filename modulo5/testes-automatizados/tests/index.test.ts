5453eb6aaba93c932c11b7727728d633d0bf23c8
chriss3008 <christian-cardeal@hotmail.com>
2022-07-11 16:56:35 -0300

    seg

---
+++ modulo5/testes-automatizados/tests/index.test.ts
@@ -0,0 +1,55 @@
+export default interface User {
+	name: string
+	balance: number
+}
+
+function performPurchase(user: User, value: number): User | undefined {
+	if(user.balance >= value) {
+		return {
+			...user,
+			balance: user.balance - value		
+		}
+	}
+	return undefined
+}
+
+describe("testando o arquivo", () =>{
+    test("Testing balance greater than value", () => {
+        const user: User = {
+            name: "Astrodev",
+            balance: 100
+        }
+    
+        const result = performPurchase(user, 40)
+        
+        expect(result).toEqual({
+            name: "Astrodev",
+            balance: 60
+        })
+    })
+
+    test("Testing balance greater than value", () => {
+        const user: User = {
+            name: "Astrodev",
+            balance: 50
+        }
+    
+        const result = performPurchase(user, 50)
+        
+        expect(result).toEqual({
+            ...user,
+            balance: 0
+        })
+    })
+
+    test("Testing balance greater than value", () => {
+        const user: User = {
+            name: "Astrodev",
+            balance: 30
+        }
+    
+        const result = performPurchase(user, 50)
+        
+        expect(result).not.toBeDefined()
+    })
+})
