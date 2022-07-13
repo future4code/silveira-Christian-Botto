93b90e9843f3c91a26f2c707a43e14cffb9e3603
chriss3008 <christian-cardeal@hotmail.com>
2022-07-13 16:24:06 -0300

    ex 13.07.2022

---
+++ modulo5/testes-backend/src/router/UserRouter.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import userController from "../controller/UserController";
+
+export const userRouter = express.Router();
+
+userRouter.post("/signup", userController.signup);
+userRouter.post("/login", userController.login);
+
+userRouter.post("/users/profile/:id", userController.login);
