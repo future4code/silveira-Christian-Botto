67777d48294c96a204d6aa179f03a91c04a1dfba
chriss3008 <christian-cardeal@hotmail.com>
2022-07-19 17:14:40 -0300

    terça

--- modulo6/lotto/src/pages/MegaSenaPage/MegaSenaPage.js
+++ modulo6/lotto/src/pages/MegaSenaPage/MegaSenaPage.js
@@ -1,12 +1,39 @@
-import React from "react";
-import { DropDown, DropDownButton, DropDownMenu } from "./styled";
+import React, { useState, useEffect } from "react";
+import { MainContainer, DropDown, DropDownButton, DropDownMenu, LottoLogo, Footer, DivNumbers, DivId } from "./styled";
+import axios from "axios";
+import { BASE_URL } from "../../constants/constants"
 
 function MegaSenaPage() {
+
+  const [lotto, setLotto] = useState([])
+
+  useEffect(() => {
+    getLotto()
+  }, [])
+
+  const getLotto = () => {
+    const url = `${BASE_URL}2359`
+    axios
+      .get(url)
+      .then((response) => {
+        setLotto(response.data)
+      })
+      .catch((error) => {
+        alert(error.response.data)
+      })
+  }
+
   return (
-    <div>
-      MegaSenaPage
+    <MainContainer>
+
+      <LottoLogo>
+
+      </LottoLogo>
+
+
+
       <DropDown>
-        <DropDownButton>Mega Sena</DropDownButton>
+        <DropDownButton ><option>Mega Sena</option></DropDownButton>
         <DropDownMenu>
           <a href="/dia+de+sorte">Dia de Sorte</a>
           <a href="/loto+facil">Loto Fácil</a>
@@ -16,7 +43,33 @@ function MegaSenaPage() {
         </DropDownMenu>
       </DropDown>
 
-    </div>
+      {lotto.numeros}
+      <DivNumbers>
+        {lotto.numeros[0]}
+        {lotto.numeros[1]}
+        {lotto.numeros[2]}
+        {lotto.numeros[3]}
+        {lotto.numeros[4]}
+        {lotto.numeros[5]}
+      </DivNumbers>
+
+      <DivId>
+        concurso
+        <br></br>
+        {lotto.id} -  {lotto.data} // Consertar exibição da data
+      </DivId>
+
+
+      <Footer>
+        <br></br>
+        Este sorteio é meramente ilustrativo e não possui nenhuma ligação com a CAIXA.
+      </Footer>
+
+
+
+
+
+    </MainContainer>
   );
 }
 
--- modulo6/lotto/src/pages/MegaSenaPage/styled.js
+++ modulo6/lotto/src/pages/MegaSenaPage/styled.js
@@ -1,29 +1,41 @@
 
 import styled from "styled-components";
 
-
-
-export const DropDown = styled.div `
-position: relative;
-  display: inline-block;
-  /* DropDownButton:hover {background-color: #FF1493;}
-  :hover {DopDownMenu.display: block;} */
+// Theme
+export const MainContainer = styled.div`
+background-color: #6BEFA3;
 `
 
-export const DropDownButton = styled.button `
-  background-color: #4CAF50;
-  color: white;
+export const Footer = styled.div`
+
+`
+
+export const DivId = styled.div`
+
+`
+
+export const DivNumbers = styled.div`
+
+`
+
+// Logo Mega Sena
+export const LottoLogo = styled.div`
+`
+// Select Menu
+export const DropDownButton = styled.select `
+  background-color: #F5F5F5;
+  color: black;
   padding: 16px;
   font-size: 16px;
   border: none;
   cursor: pointer;
+   width:200px; 
 `
-
 export const DropDownMenu = styled.div `
   display: none;
   position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
+  background-color: #F5F5F5;
+  min-width: 200px;
   box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
   z-index: 1;
   a {
@@ -32,8 +44,14 @@ export const DropDownMenu = styled.div `
   text-decoration: none;
   display: block;
     };
-  a:hover {background-color: 	#FF1493};
+  a:hover {background-color: 		#D3D3D3};
 `
 
+export const DropDown = styled.div `
+position: relative;
+  display: inline-block;
+  :hover ${DropDownMenu} {display: block;}
+  :hover ${DropDownButton} {background-color: D3D3D3;}
+`
 
-
+// 
